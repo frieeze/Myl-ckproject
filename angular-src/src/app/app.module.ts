a45c839bcdf5a4178d50c2570a89ff5b83a5c094
frieeze <alexis.surbayrole@isen.yncrea.fr>
2018-06-06 14:32:40 +0200

    whaaaaaaaaaaa

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import {RouterModule, Routes} from "@angular/router";
+
+const appRoutes: Routes = [
+
+]
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
